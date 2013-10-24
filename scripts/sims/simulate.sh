20f027ec5b32c616cb4c7656c1cc78ee196af439
Brent Pedersen <bpederse@gmail.com>
2013-10-24 17:07:13 -0600

    use w param

--- scripts/sims/simulate.sh
+++ scripts/sims/simulate.sh
@@ -11,8 +11,8 @@ for ns in 10 20 40; do
             prefix=$base/ns_${ns}/rho_${rho}/w_${w}
             mkdir -p $prefix
 
-            gen_cmd="python -m clustercorr simulate --n-samples $ns --rho-min $rho --linkage $linkage $meth $prefix/sim"
-            fit_cmd="python scripts/gen_commands.py $prefix/sim.covs.txt $prefix/sim.meth.txt $prefix/ | bash"
+            gen_cmd="python -m clustercorr simulate --n-samples $ns --rho-min $rho -w $w --linkage $linkage $meth $prefix/sim"
+            fit_cmd="python scripts/gen-commands.py $prefix/sim.covs.txt $prefix/sim.meth.txt $prefix/ | bash"
             echo "set -xe; $gen_cmd ; $fit_cmd" \
                 | bsub -J $name -e logs/$name.err -o logs/$name.out
         done
