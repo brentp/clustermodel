b6ec82d3467d524ae163c7999d8d4a3b5aba774f
Brent Pedersen <bpederse@gmail.com>
2013-11-23 10:43:39 -0700

    f1.score

--- scripts/test-methods.R
+++ scripts/test-methods.R
@@ -6,11 +6,11 @@ set.seed(42)
 # TODO: test large DMR with few samples and small DMR with 10+ each.
 n_sims = 101
 n_each = 5 # number of samples from each group
-n_probes = 2
+n_probes = 3
 rho = 0.21
 p_cutoff = 0.05 / 100000 * n_probes # we do fewer tests with more probes
-effect = 0.03
-sd = 0.01
+effect = 0.02
+sd = 0.015
 
 gen_samples = function(rho, n_each, n_probes, means=c(0, 0.0), sds=c(0.01, 0.01)){
     means = rep(means, each=n_each)
@@ -32,11 +32,13 @@ covs = data.frame(case=c(rep(1, n_each), rep(0, n_each)))
 
 found.true = mclapply(1:n_sims, function(i){
     meth = gen_samples(rho, n_each, n_probes, means=c(-effect/2, effect/2), sds=c(sd, sd))
+
     covs.long = expand.covs(covs, meth)
     mm = mixed_modelr(covs.long, methylation ~ case + (1|id) + (1|CpG))$p < p_cutoff
     ge = geer(covs.long, methylation ~ case, corstr="ar", idvar="id")$p < p_cutoff
     ex = geer(covs.long, methylation ~ case, corstr="ex", idvar="id")$p < p_cutoff
     excpg = geer(covs.long, methylation ~ case, corstr="ex", idvar="CpG")$p < p_cutoff
+
     res = list(ge=ge, mm=mm, ex=ex, excpg=excpg)#, lip=lip, li=li)
     
     res$li = stouffer_liptakr(covs, meth, methylation ~ case, cor.method="spearman")$p < 0.01
@@ -65,10 +67,22 @@ found.false = rbindlist(found.false)
 
 
 
-write("method\ttpr\tfpr\ttpr\ttpr/fpr", stdout())
+write("method\ttpr\tfpr\tppv\tprecision\tf1.score", stdout())
 for(k in colnames(found.true)){
-    tpr = sum(found.true[[k]]) / n_sims
-    fpr = sum(found.false[[k]]) / n_sims
-    write(sprintf("%s\t%.4f\t%.4f\t%.4f", k, tpr, fpr, tpr/fpr)
+    tp = sum(found.true[[k]])
+    fn = n_sims - tp
+
+    fp = sum(found.false[[k]])
+    # recall == tpr
+    # precision == number of correct results divided by the number of results that should have been returned.
+    tpr = tp / n_sims
+    fpr = fp / n_sims
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    f1.score = 2 * (precision * recall) / (precision + recall)
+
+    write(sprintf("%s\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f", k, tpr, fpr, tpr/(tpr + fpr),
+                  precision, f1.score)
     , stdout())
 }    
