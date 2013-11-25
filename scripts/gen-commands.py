7fca446392ed7cdaea53a1af2136ac5b5bc1b7ca
Brent Pedersen <bpederse@gmail.com>
2013-11-25 09:02:18 -0700

    script to compare methods on tcga data

--- scripts/gen-commands.py
+++ scripts/gen-commands.py
@@ -4,20 +4,48 @@ methods on a cluster in order to compare them.
 """
 import os
 import sys
+import pandas as pd
+import numpy as np
 
-base_model="methylation ~ case"
+np.random.seed(42)
+
+def shuffle_expr(fexpr):
+    """
+    break the relation between expression and methylation.
+    """
+    df = pd.read_table(fexpr, index_col=0)
+    orig_cols = list(df.columns)
+    cols = np.array(df.columns)
+    np.random.shuffle(cols)
+    df.columns = cols
+    new_f = fexpr.replace('.txt', '.shuffled.txt')
+    df[orig_cols].to_csv(new_f, sep="\t", index=True,
+            quote=False, float_format="%.4f")
+    return new_f
+
+
+base_model = "methylation ~ 1"
 
 covs = sys.argv[1]
 meth = sys.argv[2]
-out  = sys.argv[3]
+
+orig_expr = "/proj/Schwartz/brentp/2013/tcga-methex/brca-expr.matrix.txt"
+fake_expr = shuffle_expr(orig_expr)
+
+
+expr_locs = "/proj/Schwartz/brentp/2013/tcga-methex/expr-probe-locs.bed"
+
 
 group = os.path.splitext(os.path.basename(covs))[0]
 sds = ""
 
 extra = "| bsub -J {name} -e logs/{name}.err -o logs/{name}.out -M 2000000"
 
-base_cmd = "echo 'python -m clustermodel \"{model}\" {covs} {meth} {method} {sds} > {out}/{name}.{group}.pvals.bed "
-base_cmd += "'" + extra
+base_cmd = ("echo 'python -m clustermodel \"{model}\" {covs} {meth} {method} {sds}"
+           " --X {expr} --X-locs {expr_locs} --X-dist 50000"
+           " > {out}/{name}.{group}.pvals.bed "
+           "'" + extra)
+
 
 # switch for skat # methylatoin ~ disease + age => disease + age
 sk_model = base_model.split("~")[1].strip()
@@ -25,25 +53,33 @@ sk_model = base_model.split("~")[1].strip()
 sk_model = sk_model.split()
 sk_model = sk_model[0] + " ~ " + (" + ".join(t for t in sk_model[1:] if t != "+") or "1")
 
-for name in ("liptak", "bumping"):
-    model = base_model
-    method = "--" + name
-    print base_cmd.format(**locals())
+for expr in (orig_expr, fake_expr):
+    out = sys.argv[3] + ("fake/" if "shuff" in expr else "real/")
+    try:
+        os.makedirs(out)
+    except OSError:
+        pass
 
-print base_cmd.format(name="skat", method="--skat", model=sk_model,
-        group=group, out=out, sds=sds, covs=covs, meth=meth, extra=extra)
-
-for isds in (0, 3):
-    sds = "--outlier-sds %i" % isds
-    method = ""
-
-    model = base_model + " + (1|CpG) + (1|id)"
-    name = "both_intercept-sds_%i" % isds
-    print base_cmd.format(**locals())
-
-    for gee in ('ar,id', 'ex,CpG'):
-        name = "gee-" + gee.replace(',', '-') + ("-sds_%i" % isds)
+    for name in ("liptak", "bumping"):
         model = base_model
-        method = "--gee-args " + gee
+        method = "--" + name
         print base_cmd.format(**locals())
+
+    print base_cmd.format(name="skat", method="--skat", model=sk_model,
+            group=group, out=out, sds=sds, covs=covs, meth=meth, extra=extra,
+            expr=expr, expr_locs=expr_locs)
+
+    for isds in (0, 3):
+        sds = "--outlier-sds %i" % isds
+        method = ""
+
+        model = base_model + " + (1|CpG) + (1|id)"
+        name = "both_intercept-sds_%i" % isds
+        print base_cmd.format(**locals())
+
+        for gee in ('ar,id', 'ex,id'):
+            name = "gee-" + gee.replace(',', '-') + ("-sds_%i" % isds)
+            model = base_model
+            method = "--gee-args " + gee
+            print base_cmd.format(**locals())
 
